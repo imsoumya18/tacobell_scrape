4b501b8bc7b0a9268d891b8b30991b45b8af495b
imsoumya18 <soumyadeep184@gmail.com>
2021-03-06 12:36:18 +0530

    final

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('cred.json', scope)
 
 client = gspread.authorize(creds)
 
-ip = input('Enter input file name(without file extension):')
-op = input('Enter output file name(without file extension):')
+ip = input('Enter input file name(without file extension): ')
+op = input('Enter output file name(without file extension): ')
 
 iput = client.open(ip).sheet1
 oput = client.open(op).sheet1
 
 driver = webdriver.Chrome('chromedriver.exe')
 
-# If 'htps://tacobell.com/locations' is not censored in your location, comment the 2 following lines(line 24 & 25)
-
+# If 'htps://tacobell.com/locations' is not censored in your location, comment the 2 following lines(line 23 & 24)
 driver.get('https://chrome.google.com/webstore/detail/touch-vpn-secure-and-unli/bihmplhobchoageeokmgbdihknkjbknd')
 input('Enter:')
 
@@ -31,6 +30,12 @@ oput.update_cell(1, 4, 'Property Zip')
 
 i = 2
 j = 2
+x = 2
+while oput.cell(x, 1).value is not None or oput.cell(x+1, 1).value is not None:
+    x += 1
+    if oput.cell(x, 1).value is None and oput.cell(x+1, 1).value is not None:
+        i += 1
+        j = x + 1
 while iput.cell(i, 1).value is not None:
     driver.get('https://tacobell.com/locations')
     driver.find_element_by_id('id_store').send_keys(iput.cell(i, 1).value)
